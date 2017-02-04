067e2bcb9e13bd8fd4ed4848ba8e61176e5bf7ab
jdoss003 <jdoss003@ucr.edu>
2017-02-03 19:31:46 -0800

    Initial commit

---
+++ headers/Parser.h
@@ -0,0 +1,33 @@
+/******************************************************************************
+ *  rshell
+ *  Authors: Derek A. Sayler, Justin Doss
+ *  Copyright (C) 2017
+ *
+ *  This project is an academic assignment (UCR CS 100) and all code
+ *  is original work of the authors above. Anyone who copies this project
+ *  and submits it as their own academic work is in violation of academic
+ *  integrity.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ***************************************************************************/
+
+#ifndef RSHELL_PARSER_H
+#define RSHELL_PARSER_H
+
+class Parser
+{
+
+};
+
+#endif //RSHELL_PARSER_H
---
+++ makefile
@@ -0,0 +1,37 @@
+CC=g++
+CC_FLAGS=-Wall -Werror -ansi -pedantic
+EXEC=rshell
+
+SRCDIR=src
+BINDIR=bin
+LIBDIR=$(BINDIR)/libs
+
+SOURCES:=$(wildcard $(SRCDIR)/*.cpp) \
+    $(wildcard $(SRCDIR)/tasks/*.cpp)
+OBJECTS:=$(SOURCES:$(SRCDIR)/%.cpp=$(LIBDIR)/%.o)
+DEPENDS:=$(OBJECTS:.o=.d)
+
+all: $(EXEC)
+
+$(EXEC): $(OBJECTS)
+	$(CC) -o $(BINDIR)/$@ $^
+
+-include $(DEPENDS)
+
+$(LIBDIR)/%.o : $(SRCDIR)/%.cpp
+	mkdir -p $(@D)
+	$(CC) $(CC_FLAGS) -c $< -o $@
+	$(CC) $(CC_FLAGS) $< -MM -MT $@ > $(@:.o=.d)
+
+$(LIBDIR)/tasks/%.o : $(SRCDIR)/tasks/%.cpp
+	mkdir -p $(@D)
+	$(CC) $(CC_FLAGS) -c $< -o $@
+	$(CC) $(CC_FLAGS) $< -MM -MT $@ > $(@:.o=.d)
+
+.PHONY: clean
+clean:
+	rm -f $(OBJECTS) $(EXEC)
+
+.PHONY: cleandep
+cleandep:
+	rm -f $(DEPENDS)
