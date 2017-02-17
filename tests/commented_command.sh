ed77ef960a73a3002e583de4c6e01c3ec01364f2
jdoss003 <jdoss003@ucr.edu>
2017-02-17 12:11:50 -0800

    Test script templates

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -1,1 +1,10 @@
 #!/bin/sh
+
+cd "$(dirname "$0")"
+
+if [ ! -d "../bin" ]; then
+  ../make
+fi
+
+../bin/rshell << 'EOF'
+EOF
--- tests/single_command.sh
+++ tests/single_command.sh
@@ -1,1 +1,14 @@
 #!/bin/sh
+
+cd "$(dirname "$0")"
+
+if [ ! -d "../bin" ]; then
+  ../make
+fi
+
+../bin/rshell << 'EOF'
+echo test;
+echo second
+echo "quotes"
+exit
+EOF
